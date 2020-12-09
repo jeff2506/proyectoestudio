55bad87aac9e95c13dff5f33f509dddfeada2e6a
jeff2506 <unityuser3d@gmail.com>
2020-12-09 19:01:12 -0400

    cambios menores

---
+++ registar.php
@@ -0,0 +1,25 @@
+<?php
+
+include ("validar.php");
+
+$usuario = $_POST['usuario'];
+$contraseña = $_POST['contraseña'];
+
+$conexion = mysqli_connect('localhost', 'root', '','Registro');
+
+$insetar = "insert into usuarios (nombre_usuario, contraseña) values ('$usuario','$contraseña')";
+$ejecutar = mysqli_query($conexion,$insetar);
+
+if($ejecutar)
+{
+    echo "registrado";
+    header('location:pagina-principal.html');
+}
+else
+{
+    echo"error";
+}
+
+
+mysqli_free_result($ejecutar);
+mysqli_close($conexion);
